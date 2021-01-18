d98e02e8eaaaf188df3f852bc5f67331ecda3cc4
Mikey P <peraram2@gmail.com>
2021-01-17 16:28:22 -0800

    Outline/Notes/ predictions

---
+++ Assets/script.js
@@ -0,0 +1,31 @@
+// AS A coding boot camp student
+// I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
+// SO THAT I can gauge my progress compared to my peers
+
+
+// ```
+// GIVEN I am taking a code quiz
+// WHEN I click the start button
+        //while loop
+// THEN a timer starts and I am presented with a question
+
+// WHEN I answer a question (for Loop, create an array of randomly assigned questions and then iterate)
+    //How to make a question...
+    //create objects with propoerties: question, answer w,x,y,z are booleons
+    //on click check to see if it is true, 
+    // else take away seconds from timer
+// THEN I am presented with another question
+
+
+// WHEN I answer a question incorrectly
+// THEN time is subtracted from the clock
+
+// WHEN all questions are answered or the timer reaches 0
+        //nest the game in a while loop and return when time is expired
+// THEN the game is over
+
+// WHEN the game is over
+// THEN I can save my initials and my score
+        //use the cache to create object with score and intials consecutive correct questions?
+// ```
+
